e775dc5a8c55cbf03ac22825fbd15cf7bc4bca6e
Andy Willekens <andywillekens@gmail.com>
2024-02-23 22:21:36 +0100

    Added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,35 @@
+import { resolve } from 'path'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', 'nuxt-icon']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', 'nuxt-icon'],
+  alias: {
+    '@': resolve(__dirname, './')
+  },
+  tailwindcss: {
+    cssPath: '~/styles/tailwind.scss'
+  },
+  image: {
+    screens: {
+      xs: 320,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536,
+      '2xl': 1536
+    }
+  }
+  // app: {
+  //   head: {
+  //     templateParams: {
+  //       separator: '-'
+  //     },
+  //     charset: 'utf-8',
+  //     link: [
+  //       { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+  //       { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }
+  //     ]
+  //   }
+  // },
 })
